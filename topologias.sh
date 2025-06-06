d9f9e93c57ee51329187eb1d864d602d77c51eac
lesssC <leslie.cabrera.luey@gmail.com>
2025-06-06 01:25:53 -0400

    version raiz

---
+++ topologias.sh
@@ -0,0 +1,9 @@
+#Comandos para distintas topologias
+#1. TOpologia arbol
+sudo mn --topo tree,4,3
+#2. Topologia lineal
+sudo min --topo linear,5,4
+#3. Topologia torus
+sudo mn --topo torus,3,4,5
+
+
